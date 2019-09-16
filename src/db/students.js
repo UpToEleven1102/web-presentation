c29d8e8b1a81a456ff9e97b62890767e54286d22
nnhuyen <huyen.nguyen@ttu.edu>
2019-09-16 14:45:46 -0500

    update url

--- src/db/students.js
+++ src/db/students.js
@@ -2,9 +2,9 @@ const db = require('./index');
 const scoreDB = require('./scores')
 
 let data = [
-    {id: 1, name: 'Huyen', avatar: 'https://source.unsplash.com/random/1', image: 'https://source.unsplash.com/random/1', url: 'https://en.wikipedia.org/wiki/History_of_display_technology'},
-    {id: 2, name: 'Ngoc', avatar: 'https://source.unsplash.com/random/2', image: 'https://source.unsplash.com/random/2', url: 'https://en.wikipedia.org/wiki/Llangeinor'},
-    {id: 3, name: 'Contigo', avatar: 'https://source.unsplash.com/random/3', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif', url: 'https://en.wikipedia.org/wiki/Gardenia_remyi'},
+    {id: 1, name: 'Stash', avatar: 'https://source.unsplash.com/random/1', image: 'https://source.unsplash.com/random/1', url: 'https://nnhuyen.github.io/HCI-HW1-Elevator/'},
+    {id: 2, name: 'Commits', avatar: 'https://source.unsplash.com/random/2', image: 'https://source.unsplash.com/random/2', url: 'https://en.wikipedia.org/wiki/Llangeinor'},
+    {id: 3, name: 'Merge', avatar: 'https://source.unsplash.com/random/3', image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif', url: 'https://en.wikipedia.org/wiki/Gardenia_remyi'},
     {id: 4, name: 'Gardenia', avatar: 'https://source.unsplash.com/random/4', image: 'https://source.unsplash.com/random/4', url: 'https://en.wikipedia.org/wiki/Streetcars_in_Reno'},
     {id: 5, name: 'Barrett', avatar: 'https://source.unsplash.com/random/5', image: 'https://source.unsplash.com/random/5', url: 'https://en.wikipedia.org/wiki/K._K._Barrett'},
 
